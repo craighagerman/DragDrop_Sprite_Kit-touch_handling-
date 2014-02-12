71cd296e81ed428df3cdbb64c112e6e1dffd2367
Craig Hagerman <craighagerman@gmail.com>
2014-02-12 13:25:28 -0500

    initial commit

---
+++ DragDrop/MyScene.m
@@ -0,0 +1,143 @@
+//
+//  MyScene.m
+//  DragDrop
+//
+//  Created by Craig Hagerman on 2/9/14.
+//  Copyright (c) 2014 ece1778. All rights reserved.
+//
+
+#import "MyScene.h"
+
+static NSString * const kAnimalNodeName = @"movable";   // to identify movable nodes
+
+@interface MyScene ()
+
+@property (nonatomic, strong) SKSpriteNode *background;
+@property (nonatomic, strong) SKSpriteNode *selectedNode;
+
+@end
+
+@implementation MyScene
+
+- (id)initWithSize:(CGSize)size {
+    if (self = [super initWithSize:size]) {
+        
+        // 1) Loading the background
+        _background = [SKSpriteNode spriteNodeWithImageNamed:@"blue-shooting-stars"];
+        [_background setName:@"background"];
+        [_background setAnchorPoint:CGPointZero];       // set the anchor point to the lower left corner
+        [self addChild:_background];
+        
+        // 2) Loading the images
+        NSArray *imageNames = @[@"bird", @"cat", @"dog", @"turtle"];
+        for(int i = 0; i < [imageNames count]; ++i) {
+            NSString *imageName = [imageNames objectAtIndex:i];
+            SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:imageName];
+            [sprite setName:kAnimalNodeName];
+            
+            float offsetFraction = ((float)(i + 1)) / ([imageNames count] + 1);
+            [sprite setPosition:CGPointMake(size.width * offsetFraction, size.height / 2)];
+            [_background addChild:sprite];
+        }
+    }
+    
+    return self;
+}
+
+/*
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    // Called when a touch begins
+    
+    for (UITouch *touch in touches) {
+        CGPoint location = [touch locationInNode:self];
+        
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+        
+        sprite.position = location;
+        
+        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+        
+        [sprite runAction:[SKAction repeatActionForever:action]];
+        
+        [self addChild:sprite];
+    }
+}
+*/
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint positionInScene = [touch locationInNode:self];
+    [self selectNodeForTouch:positionInScene];
+}
+
+
+// get the touch from the touches set. Then convert the touch location to the location in a specific node
+- (void)selectNodeForTouch:(CGPoint)touchLocation {
+    SKSpriteNode *touchedNode = (SKSpriteNode *)[self nodeAtPoint:touchLocation];
+    
+    // check if node is a new node, or the one previously touched (in which case do nothing)
+	if(![_selectedNode isEqual:touchedNode]) {
+		[_selectedNode removeAllActions];
+		[_selectedNode runAction:[SKAction rotateToAngle:0.0f duration:0.1]];
+        
+		_selectedNode = touchedNode;
+		// check to see if node can animatable (can wiggle)
+		if([[touchedNode name] isEqualToString:kAnimalNodeName]) {
+			SKAction *sequence = [SKAction sequence:@[[SKAction rotateByAngle:degToRad(-4.0f) duration:0.1],
+													  [SKAction rotateByAngle:0.0 duration:0.1],
+													  [SKAction rotateByAngle:degToRad(4.0f) duration:0.1]]];
+			[_selectedNode runAction:[SKAction repeatActionForever:sequence]];
+		}
+	}
+    
+}
+
+
+// get the touch and convert it‘s position to the position in your scene.
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+	UITouch *touch = [touches anyObject];
+	CGPoint positionInScene = [touch locationInNode:self];
+	CGPoint previousPosition = [touch previousLocationInNode:self];
+    
+	CGPoint translation = CGPointMake(positionInScene.x - previousPosition.x, positionInScene.y - previousPosition.y);
+    
+	[self panForTranslation:translation];
+}
+
+
+
+
+// Sprite Kit uses radians for rotation so this function converts an angle given in degree to radians.
+float degToRad(float degree) {
+	return degree / 180.0f * M_PI;
+}
+
+
+// make sure you don’t scroll the layer beyond the bounds of the background image.
+- (CGPoint)boundLayerPos:(CGPoint)newPos {
+    CGSize winSize = self.size;
+    CGPoint retval = newPos;
+    retval.x = MIN(retval.x, 0);
+    retval.x = MAX(retval.x, -[_background size].width+ winSize.width);
+    retval.y = [self position].y;
+    return retval;
+}
+
+
+// check if _selectedNode is an animal node and sets the position based on a passed-in translation.
+- (void)panForTranslation:(CGPoint)translation {
+    CGPoint position = [_selectedNode position];
+    if([[_selectedNode name] isEqualToString:kAnimalNodeName]) {
+        [_selectedNode setPosition:CGPointMake(position.x + translation.x, position.y + translation.y)];
+    } else {
+        CGPoint newPos = CGPointMake(position.x + translation.x, position.y + translation.y);
+        [_background setPosition:[self boundLayerPos:newPos]];
+    }
+}
+
+
+-(void)update:(CFTimeInterval)currentTime {
+    /* Called before each frame is rendered */
+}
+
+@end
---
+++ DragDrop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DragDrop
+//
+//  Created by Craig Hagerman on 2/9/14.
+//  Copyright (c) 2014 ece1778. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
